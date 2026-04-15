2a0108077286e1e2aa6427afe50266b0a185374b
Mudassir Markania <mudassir.markania@outlook.com>
2026-04-15 14:35:34 +0530

    fix: update CORS configuration to allow requests from localhost

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ import { startDeadlineChecker } from "./jobs/deadlineChecker.js";
 
 const app = express();
 app.use(helmet());
-app.use(cors({ origin: "https://task.nexcorealliance.com/", credentials: true }));
+app.use(cors({ origin: ["https://task.nexcorealliance.com", "http://localhost:3000"], credentials: true }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
